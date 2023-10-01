96936dd528df381c82456ae0780b92e3a98b45fb
Shayan Shaikh <shaikhshayan137@gmail.com>
2023-10-01 10:34:49 +0500

    created system statistics script

---
+++ system_stats.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Author: Shayan Shaikh
+
+# Title
+echo "*************************** SHELL SCRIPTING ***************************"
+
+# Ask for user's name
+echo "Please Enter Your Name: "
+read name
+echo
+
+# Greeting
+echo "********* Hi $name - Please see the detailed server status below *********"
+echo " ***************************************************************************"
+echo
+
+# Display Current Date and Time
+echo " **************************** Current Date and Time *************************"
+date | awk ' {print "Today is: " $3 " - " $2 " ; Day = "$1 " ; Time: " $4 }'
+echo
+
+# Disk Space Available
+echo " *************************** Disk Space Available **************************"
+df -H | xargs | awk '{print "Disk Space Available: " "Free Used: "$10 "/" $9 " :GB" }'
+echo
+
+# User Uptime
+echo " ******************************* User Uptime *******************************"
+uptime | awk '{print $1 " / " $3 "" $4}'
+echo
+
+# Last 3 Login Details
+echo " ************************ Last 3 Login Details **************************"
+last | head -3
+echo
+
+# Currently Connected User
+echo " **************************** Currently Connected *************************"
+whoami
+echo
+
+# End
+echo "******************************************* END *****************************"
+
